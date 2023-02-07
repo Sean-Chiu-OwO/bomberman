2bf3dfa333d90f9d6b26cf8b06e688bef58ea106
sean9 <sean9@SEAN-NB>
2023-02-07 17:45:38 +0800

    加入專案檔案。

---
+++ bomberman/Explosion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Tab;
+
+namespace bomberman
+{
+    internal class Explosion    //爆炸
+    {
+        public static readonly int ExplosionHeight = 70, ExplosionWidth = 70;
+        public PictureBox boxExplosion = new PictureBox();
+        public static readonly int duration = 1;    //爆炸持續時間
+
+        public Explosion()
+        {
+            this.boxExplosion.Size = new Size(ExplosionHeight, ExplosionWidth); //設定爆炸大小
+            this.boxExplosion.Image = Properties.Resources.explosion;           //設定爆炸圖案
+            this.boxExplosion.Tag = "explosion";
+        }
+
+        public void Spawn(int posX, int posY)
+        {
+            this.boxExplosion.Location = new Point(posX, posY);
+            //this.boxExplosion.BringToFront();
+        }
+    }
+}
